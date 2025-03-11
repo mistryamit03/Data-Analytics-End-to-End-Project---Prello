024618b31f7bf236f0c00378af6a53711be30ce1
Franz van Ackeren <franz.ackeren@googlemail.com>
2025-03-11 15:01:44 +0000

    added deparment code and name to int_economic

--- models/intermediate/int_econmic_by_region.sql
+++ models/intermediate/int_econmic_by_region.sql
@@ -3,7 +3,9 @@ SELECT
     pbm.year_date,
     pbm.population AS total_population,
     IFNULL(asm.avg_net_salary, 0) AS avg_net_salary,
-    ppm.population AS poverty_population
+    ppm.population AS poverty_population,
+    c.department_code,
+    c.department_name
 FROM
     {{ ref('stg_raw__population_by_municipality') }} pbm 
 LEFT JOIN
@@ -14,4 +16,4 @@ LEFT JOIN
     {{ ref("stg_raw__poverty_population_by_municipality") }} ppm
         ON pbm.municipality_code = ppm.municipality_code
         AND pbm.year_date = ppm.year_date
-
+Left join {{ref('stg_raw__geographical_referential')}} as c ON pbm.municipality_code = c.municipality_code 
