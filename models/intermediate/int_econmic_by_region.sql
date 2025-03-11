dc9e9f62f57417b04578364c41516d1fdc394302
Aleksandr Gulianskii <gulyanskiyas@gmail.com>
2025-03-11 11:50:48 +0000

    first int_economic table

--- models/intermediate/int_econmic_by_region.sql
+++ models/intermediate/int_econmic_by_region.sql
@@ -2,15 +2,16 @@ SELECT
     pbm.municipality_code,
     pbm.year,
     pbm.population AS total_population,
-    asm.avg_net_salary,
+    IFNULL(asm.avg_net_salary, 0) AS avg_net_salary,
     ppm.population AS poverty_population
 FROM
-    {{ source('population_by_municipality') }} pbm 
+    {{ ref('stg_raw__population_by_municipality') }} pbm 
 LEFT JOIN
-    {{ source('average_salary_by_municipality') }} asm
+    {{ ref('stg_raw__average_salary_by_municipality') }} asm
         ON pbm.municipality_code = asm.municipality_code
         AND pbm.year = asm.year
 LEFT JOIN
-    {{ source('poverty_population_by_municipality') }} ppm
+    {{ ref("stg_raw__poverty_population_by_municipality") }} ppm
         ON pbm.municipality_code = ppm.municipality_code
         AND pbm.year = ppm.year
+
